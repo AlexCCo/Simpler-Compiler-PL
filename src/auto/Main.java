4605f1118e27ca2acd532e279041e60f62d091e3
togo347 <togo347@gmail.com>
2020-02-24 00:05:34 +0100

    fixes

--- src/auto/Main.java
+++ src/auto/Main.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         //Reader input = new InputStreamReader(new FileInputStream(args[0]));
-        Reader input = new InputStreamReader(new FileInputStream("ejemplos/ej5.alex"));
+        Reader input = new InputStreamReader(new FileInputStream("examples/ej5.alex"));
         LexicalScanner al = new LexicalScanner(input);
         LexicalUnit unidad;
         do {
--- src/manual/Main.java
+++ src/manual/Main.java
@@ -14,7 +14,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         //Reader input = new InputStreamReader(new FileInputStream(args[0]));
-        Reader input = new InputStreamReader(new FileInputStream("ejemplos/ejemplo.alex"));
+        Reader input = new InputStreamReader(new FileInputStream("examples/ejemplo.alex"));
         LexicalScanner al = new LexicalScanner(input);
         LexicalUnit unidad;
         do {
