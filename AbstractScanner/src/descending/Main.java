cade6e9bcba8131ac1b004e770bcc7367d3df30e
Tomas <tomas.golomb@atani.com>
2020-05-03 20:37:20 +0200

    finished abstract structure

--- AbstractScanner/src/descending/Main.java
+++ AbstractScanner/src/descending/Main.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 public class Main{
    public static void main(String[] args) throws Exception {
       Evaluador eval;
-      eval = new Evaluador(new FileReader("AbstractScanner/src/examples/ejemplo.alex"));
+      eval = new Evaluador(new FileReader("AbstractScanner/src/descending/examples/ejemplo.alex"));
       System.out.println(eval.Sp());
    }
 }
